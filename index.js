68eb3dc5dcf18d88f93c53e2a6c4a7af7f9ccdb2
dev-vickie <vmutethia84@gmail.com>
2023-03-31 16:51:56 +0300

    remove node: before http

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("node:http");
+const http = require("http");
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, { "Content-Type": "text/plain" });
