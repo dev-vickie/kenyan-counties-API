43c7461677355468744ce1065977a32babf73955
dev-vickie <vmutethia84@gmail.com>
2023-04-01 09:46:29 +0300

    fix: change response content

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
+  
   fs.readFile("./counties.json", (err, data) => {
     if (err) {
       response.writeHead(500, { "Content-Type": "text/plain" });
